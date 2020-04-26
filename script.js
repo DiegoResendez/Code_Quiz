50bec742ad4f8510727af9af4f7c95742daea33a
unknown <diego.resendez@yahoo.com>
2020-04-26 14:20:30 -0700

    Sunday Upload

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ const buttonB = document.getElementById("buttonB")
 const buttonC = document.getElementById("buttonC")
 const buttonD = document.getElementById("buttonD")
 const startScreen = document.getElementById("startScreen")
-const submitButton = document.getElementById("buttonSubmit")
+const submitButton = document.getElementById("add-score")
 const scoreboardElement = document.getElementById("scoreboard")
+const userName = document.getElementById("user-input")
+const viewHighScoresButton = document.getElementById("viewHighScores")
+const goBackButton = document.getElementById("go-back")
+const deleteHighScoresButton = document.getElementById("delete-highscores")
+const leaderBoardElement = document.getElementById("leaderBoard")
+const scoreInputElement = document.getElementById("score-input")
 
 // pointers to scoreboard elements (button, text)
+// whatever button you create in index.html after line 61
+// pointer to viewHighScores button
 
 let questionNumber = 0;
 let timerStart = 60;
@@ -20,32 +28,62 @@ let interval;
 // const currentQuestionIndex
 
 // create an event listener for the score submit button
+goBackButton.addEventListener("click", )//refresh or go back to start
+viewHighScoresButton.addEventListener("click", renderScoreboard)
+deleteHighScoresButton.addEventListener("click", clearScoreboard)
+submitButton.addEventListener("click", loadScore)
 // in the event listener, you want to add in the algorithm for saving to local storage
 // window.location.reload();
 
+
+// create an event listener for clearHighScores button
+
+// create an event listener for viewHighScores button that renders the scoreboard to the page
+
+// create a function to render the scoreboard
+function renderScoreboard() {
+
+    // load scoreboard from localStorage (localStorage.getItem)
+    // JSON.parse to convert it back into an array
+
+    // use that array to render a list for your scoreboard
+
+}
+
+function loadScore(){
+    // add score to local storage create div for repository
+
+}
+
+// delete highscore function
+function clearScoreboard() {
+    localStorage.setItem("Scoreboard", "[]")
+}
+
 startButton.addEventListener("click", startGame)
 // submitButton.addEventListener("click", adjustScoreboard)
-// submitButton.addEventListener("click", loadScore)
+submitButton.addEventListener("click", setScore)
 let answerButton = document.getElementsByClassName("buttonAnswer")
  for( let i = 0; i < answerButton.length; i++){
      answerButton[i].addEventListener("click", function(event){
          let answer = this.getAttribute("data")
-         if (answer === false){
-            timerElement = timerElement - 10;
+         console.log(answer)
+         if (answer === "false"){
+            timerStart = timerStart - 10;
+            console.log("if false")
          } 
          if(questionNumber < question.length){
-         questionNumber++;
+         console.log(questionNumber)
          setNextQuestion()
         } else{
             // endGame
             // clearInterval(interval)
             stopTimer();
-
-
             // show your scoreboard?
-            
+            scoreboardElement.classList.remove("hide");
+            questionContainerElement.classList.add("hide");
         }
-        //  console.log(timerElement)
+         console.log(timerElement)
      }) 
  }
  
@@ -67,10 +105,7 @@ function startGame() {
 function setNextQuestion() {
     showQuestion(questionNumber)
     console.log ("Should set next question")
-    if (questionNumber > question.length){
-        scoreboardElement.classList.remove("hide");
-        questionContainerElement.classList.add("hide");
-    }
+    questionNumber++;
 }
 
 function showQuestion(questionNumber) {
@@ -98,8 +133,8 @@ function startTimer() {
 }
 
 function stopTimer() {
-    if(timerElement = 0){
-        clearInterval (interval)
+    if(timerStart === 0){
+        clearInterval(interval)
     } else
     clearInterval(interval);
 
@@ -111,14 +146,17 @@ function getNextQuestion(){
     questionOnScreen
 }
 
-function loadScore(){
-    // add score to local storage create div for repository
-}
 
-function setScore (finalScore){
-    if (timerElement = 0) {
-        setScore == 0
-    } else (setScore == timerElement)
+
+function setScore (){
+    let finalScore = {name: userName.value, score: timerStart};
+let scoreboard = localStorage.getItem("Scoreboard");
+if(scoreboard){
+    scoreboard = JSON.parse(scoreboard)
+    scoreboard.push(finalScore);
+} else(scoreboard = [finalScore]);
+
+localStorage.setItem("Scoreboard", JSON.stringify(scoreboard))
 }
 
 
